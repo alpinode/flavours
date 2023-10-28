861be09ba4f392304940f96f63effa6dc2da8b86
Slee Woo <mail@sleewoo.com>
2023-10-28 17:22:23 +0300

    chore

--- shared/dev/build/tmux.sh
+++ shared/dev/build/tmux.sh
@@ -1,6 +1,7 @@
 
 set -e
 
+# TODO: install tmux using apk when tmuxinator accepts tmux version 3.4
 apk add --no-cache \
   libevent-dev \
   ncurses-dev \
@@ -9,6 +10,7 @@ apk add --no-cache \
 v="3.3a"
 wget https://github.com/tmux/tmux/releases/download/$v/tmux-$v.tar.gz -O- | tar -xz -C /tmp
 cd /tmp/tmux-$v
+
 ./configure && make && make install
 
 rm -fr /tmp/tmux-$v
